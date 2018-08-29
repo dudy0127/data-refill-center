1861b74eb3f3b34795ef41616498aff4740b93d2
Dudy <1796244932@qq.com>
2018-08-30 00:07:27 +0800

    init

---
+++ src/main/java/io/github/dudy/transaction/datarefillcenter/DataRefillCenterApplication.java
@@ -0,0 +1,12 @@
+package io.github.dudy.transaction.datarefillcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRefillCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRefillCenterApplication.class, args);
+	}
+}
